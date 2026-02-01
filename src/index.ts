4cfa5be589806b3fe11b26cc02eb8c6098758465
MuneebM07 <muneeb.dev01@gmail.com>
2026-02-01 17:17:34 +0530

    ws authentication

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,14 @@ app.use(express.json())
 connectDB();
 
 
+app.use("/auth", router);
 
 const server = http.createServer(app);
 setupWebSocket(server);
 
 server.listen(3000, ()=>{
     console.log("Http + websocket running on port 3000")
-})
+});
+
 
  
--- src/ws.ts
+++ src/ws.ts
@@ -1,17 +1,61 @@
 import { WebSocketServer } from 'ws';
 import type { Server } from 'http';
 
+import jwt from 'jsonwebtoken'
+import { decode } from 'punycode';
 export function setupWebSocket(server: Server) {
 
 
+    const JWT_PASSWORD = process.env.JWT_PASSWORD as string
     const wss = new WebSocketServer({
         server,
         path: "/ws"
 
     });
-    wss.on("connection", (ws) => {
-        console.log("WebSocket client connected");
+    wss.on("connection", (ws,req) => {
+
+        try{
+                    const url = new URL(req.url!, "http://localhost");
+        const token = url.searchParams.get("token");
+
+        if(!token){
+
+            ws.send(JSON.stringify({
+                event:"Error", 
+                data: {message: "Unauthorized or invalid token"}
+            }));
+            ws.close();
+            return;
+        }
+        const decoded = jwt.verify(token, JWT_PASSWORD);
+
+        if(typeof decoded === "string"){
+            ws.send(JSON.stringify({
+                event: "error", 
+                data : {message: "Unauthorized or invalid token"}
+            }))
+            ws.close();
+            return;
+        }
+        (ws as any).user= {
+            userId: decoded.userId, 
+            role:  decoded.role
+        };
+        console.log("Web Socket authenticated ", (ws as any).user);
+
+
+
+        }catch{
+            ws.send(JSON.stringify({
+                event: "error", 
+                data: {message: "Unauthorized or invalid token"}
+            }));
+            ws.close();
+        }
     });
+
+
+
 
 }
 
